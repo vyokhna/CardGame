08905eca4f25d704a6cfe94a68ea7ad4599cddc2
ganzerga <vyokhna@malkosua.com>
2015-12-01 17:23:28 +0200

    final version

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -6,11 +6,15 @@ cardApp.controller('mainCtrl', ['$scope', '$timeout', function($scope, $timeout)
     $scope.gameOver = false;
     $scope.moveMade = false;
     $scope.movesCount = 0;
+    $scope.matchedPairs = 0;
 
     //number of cards in half of deck
-    $scope.halfDeckVolume = 2;
+    $scope.halfDeckVolume = 26;
 
     $scope.createDeck = function () {
+        $scope.gameOver = false;
+        $scope.movesCount = 0;
+        $scope.matchedPairs = 0;
 
         //declaring arrays of available suits and values
         var suits = ['♠', '♥', '♦', '♣'],
@@ -41,62 +45,40 @@ cardApp.controller('mainCtrl', ['$scope', '$timeout', function($scope, $timeout)
 
     $scope.revealCard = function(card){
 
-        //exit function if the player is making a move
-        if($scope.moveMade) return;
+        //exit function if the player is making a move or clicking on revealed card
+        if($scope.moveMade || card.revealed) return;
 
-        //event fires only on unmatched cards
-        if(!card.matched){
+        //we reveal clicked card
+        $scope.deckArray[card.id].revealed = true;
 
-            //we reveal clicked card
-            $scope.deckArray[card.id].revealed = true;
+        //check if we can add a card to current move
+        if($scope.cardPair.length < 2) $scope.cardPair.push(card);
 
-            //check if we can add a card to current move
-            if($scope.cardPair.length < 2){
-                $scope.cardPair.push(card);
+        //if cards match
+        if($scope.cardPair.length == 2 && $scope.cardPair[0].value == $scope.cardPair[1].value && $scope.cardPair[0].id != $scope.cardPair[1].id){
+            _.each($scope.cardPair, function(el){
+                $scope.deckArray[el.id].matched = true;
+            });
+            $scope.cardPair = [];
+            $scope.movesCount++;
 
-                //assigning conditions to variables
-                var ifTwo = $scope.cardPair.length == 2,
-                    ifEqual = $scope.cardPair[0].value == $scope.cardPair[1].value,
-                    ifNotSame = $scope.cardPair[0].id != $scope.cardPair[1].id;
+            //incrementing the value of matched pairs
+            $scope.matchedPairs++;
 
-                if(ifTwo && ifEqual && ifNotSame){
-                    _.each($scope.cardPair, function(el){
-                        $scope.deckArray[el.id].matched = true;
-                    });
-                    $scope.cardPair = [];
-                    $scope.movesCount++;
+            //checking if this turn is the last one and finishing the game if it is so
+            $scope.gameOver = ($scope.deckArray.length / $scope.matchedPairs === 2);
 
-                    //creating a temporary array to store all "matched" values
-                    var arr = [];
-                    _.each($scope.deckArray, function(el){
-                            arr.push(el.matched);
-                    });
-
-                    //checking if this turn is the last one and finishing the game if it is so
-                    $scope.gameOver = _.every(arr, function(el2) {
-                        return el2 === true
-                    })
-
-                } else if(ifTwo){
-                    $scope.moveMade = true;
-                    $timeout(function(){
-                        _.each($scope.cardPair, function(el){
-                            $scope.deckArray[el.id].revealed = false;
-                        });
-                        $scope.cardPair = [];
-                        $scope.movesCount++;
-                        $scope.moveMade = false;
-                    },1000)
-
-                }
-            }
+            //if cards don't match
+        }else if($scope.cardPair.length == 2 && $scope.cardPair[0].value != $scope.cardPair[1].value && $scope.cardPair[0].id != $scope.cardPair[1].id){
+            $scope.moveMade = true;
+            $scope.movesCount++;
+            $timeout(function(){
+                _.each($scope.cardPair, function(el){
+                    $scope.deckArray[el.id].revealed = false;
+                });
+                $scope.cardPair = [];
+                $scope.moveMade = false;
+            },1000)
         }
     };
-
-    //begin the game once more
-    $scope.startOver = function(){
-        $scope.gameOver = false;
-        $scope.movesCount = 0;
-        $scope.createDeck()
-    }
 }]);
